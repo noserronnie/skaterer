0c00ec24b7574b7512a86436e134d282ab6089b5
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2022-06-14 14:15:05 +0200

    Change deck model

--- Skateshop/Skateshop/Services/Products/Models/Product.cs
+++ Skateshop/Skateshop/Services/Products/Models/Product.cs
@@ -1,10 +1,12 @@
-﻿using Skaterer.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Skaterer.Models;
 
 namespace Skaterer.Services.Products.Models
 {
     public class Product
     {
 
+        [Required]
         public string Name { get; set; }
 
         public Brand Brand { get; set; }
