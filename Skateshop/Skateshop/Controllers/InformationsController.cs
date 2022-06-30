d5be6205120f7237156f804f528be4f670f069d9
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2022-06-30 14:11:24 +0200

    Add about us side

---
+++ Skateshop/Skateshop/Controllers/InformationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skaterer.Controllers
+{
+    public class InformationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
