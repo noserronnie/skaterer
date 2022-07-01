b09a16ce5abb92f4906ff8f5b3197bef834fc486
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2022-07-01 13:42:00 +0200

    added Checkout

---
+++ Skateshop/Skateshop/Controllers/PayController.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using Skaterer.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Skaterer.Controllers
+{
+    public class PayController : Controller
+    {
+        private readonly SkatererContext _skatererContext;
+
+        public PayController(SkatererContext skatererContext)
+        {
+            _skatererContext = skatererContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Download()
+        {
+            string csv;
+            using (StreamReader sr = new StreamReader("Export.csv"))
+            {
+                csv = sr.ReadToEnd();
+            }
+            return File(new UTF8Encoding().GetBytes(csv), "text/csv", "Export.csv");
+        }
+    }
+}
